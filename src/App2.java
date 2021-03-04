d8f91fa9e0eda20443ceb55b2af0a2baca5edfcd
Diógenes Júnior <diogjunior10071@gmail.com>
2021-03-04 12:50:18 -0300

    App2 concluído

--- src/App2.java
+++ src/App2.java
@@ -2,6 +2,16 @@ public class App2 {
 
     public static void name(String[] args) {
         
-        
+        String nome;
+        nome = JOptionPane.showInputDialog(null, " Seu nome: ");
+
+        String ano;
+        ano = JOptionPane.showInputDialog(null, " Seu ano de nascimento: ");
+        int num = Integer.parseInt(ano);
+        int resultado = 2021 - num;
+
+        JOptionPane.showMessageDialog(null, "Seu nome eh " + nome);
+        JOptionPane.showMessageDialog(null, "Sua idade eh " + resultado);
+
     }
 }
